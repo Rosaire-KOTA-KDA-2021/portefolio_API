928ac48b47e2939bffac061f48b00cfc191a270a
rkota <rosairekota@gmail.com>
2021-06-16 22:28:31 +0100

    feat:add static UPDATE Method in the ProjectController Class

--- src/Controller/ProjectController.js
+++ src/Controller/ProjectController.js
@@ -18,11 +18,11 @@ class ProjectController {
       response.send(message);
     });
   }
-  // static update(request, response) {
-  //   Project.delete(request.id, request.body, (project) => {
-  //     response.send(project);
-  //   });
-  // }
+  static update(request, response) {
+    Project.delete(request.id, request.body, (project) => {
+      response.send(project);
+    });
+  }
   static delete(request, response) {
     Project.delete(request.params.id, (message) => {
       response.send(message);
