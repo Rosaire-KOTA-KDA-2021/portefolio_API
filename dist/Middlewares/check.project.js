780282402590708ac989835605d296fb834cc275
rkota <rosairekota@gmail.com>
2021-06-30 14:16:26 +0100

    refactor:solving environment variable problems and delete the .env file

--- dist/Middlewares/check.project.js
+++ dist/Middlewares/check.project.js
@@ -1,13 +1,23 @@
 "use strict";
 
 var _require = require("../../config/database"),
-    db = _require.db;
+  db = _require.db;
 
-var checkIfProjectsExist = function checkIfProjectsExist(request, response, next) {
-  db.query("SELECT * FROM project WHERE id=? LIMIT 1", [request.params.id], function (err, result) {
-    if (err) throw err;else if (result.length <= 0) return response.status(404).send("le projet est inexistant");
-    request.project = result;
-  });
+var checkIfProjectsExist = function checkIfProjectsExist(
+  request,
+  response,
+  next
+) {
+  db.query(
+    "SELECT * FROM project WHERE id=? LIMIT 1",
+    [request.params.id],
+    function (err, result) {
+      if (err) throw err;
+      else if (result.length <= 0)
+        return response.status(404).send("le projet est inexistant");
+      request.project = result;
+    }
+  );
   next();
 };
 
