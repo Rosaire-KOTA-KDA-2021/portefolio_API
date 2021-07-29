5fcca40a7dc3c879ac20d903ce6b4208550329f6
rkota <rosairekota@gmail.com>
2021-07-29 10:20:59 +0100

    fix:solving the PORT problem

--- dist/Middlewares/validate.body.js
+++ dist/Middlewares/validate.body.js
@@ -2,20 +2,18 @@
 
 var validateBody = function validateBody(req, res, next) {
   var _req$body = req.body,
-    title = _req$body.title,
-    image = _req$body.image,
-    used_technologies = _req$body.used_technologies,
-    description = _req$body.description,
-    github_link = _req$body.github_link;
+      title = _req$body.title,
+      image = _req$body.image,
+      used_technologies = _req$body.used_technologies,
+      description = _req$body.description,
+      github_link = _req$body.github_link;
   var values = [title, image, used_technologies, description, github_link];
 
-  if (
-    values.some(function (value) {
-      return value.length === 0;
-    })
-  ) {
+  if (values.some(function (value) {
+    return value.length === 0;
+  })) {
     return res.status(440).send({
-      message: "Veuillez remplir tous les champs svp",
+      message: "Veuillez remplir tous les champs svp"
     });
   }
 
